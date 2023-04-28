2e1c9a4bee1375217d6ae2e70dac61b33385047a
aljaz-ferenc <aljaz.ferenc@protonmail.com>
2023-04-28 11:19:39 +0200

    add SubmitForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,43 @@
-import { useState, useMemo, useEffect} from 'react'
-import './App.css'
+import { useState, useMemo, useEffect } from "react";
 import { getAllTemps, getTempsByDateRange } from "./firebase/firebase";
+import "./App.css";
 import Statistics from "./components/Statistics";
+import SubmitForm from "./components/SubmitForm";
 
 function App() {
   const [data, setData] = useState(null);
-  const [lastRequestTime, setLastRequestTime] = useState(null)
+  const [lastRequestTime, setLastRequestTime] = useState(null);
   const [filter, setFilter] = useState({ startDate: null, endDate: null });
-  const throttleTime = 3000
+  const throttleTime = 3000;
 
   //fetch on app render
   useMemo(() => {
-    getAllData()
-    setLastRequestTime(currentTime())
+    getAllData();
+    setLastRequestTime(currentTime());
   }, []);
 
   //filter button click
   useEffect(() => {
     if (filter.startDate && filter.endDate && checkThrottleTime()) {
       getFilteredData();
-      setLastRequestTime(currentTime())
-    }else if(checkThrottleTime()){
-      getAllData()
-      setLastRequestTime(currentTime())
+      setLastRequestTime(currentTime());
+    } else if (checkThrottleTime()) {
+      getAllData();
+      setLastRequestTime(currentTime());
     }
   }, [filter]);
 
-  function getAllData(){
+  //submit button click
+  function handleAddData() {
+    if (filter.startDate && filter.endDate) {
+      getFilteredData();
+    } else {
+      getAllData();
+      console.log("fetch");
+    }
+  }
+
+  function getAllData() {
     getAllTemps().then((data) => setData(data));
   }
 
@@ -36,25 +47,25 @@ function App() {
     );
   }
 
-  function currentTime(){
-    return new Date().getTime()
+  function currentTime() {
+    return new Date().getTime();
   }
 
-  function checkThrottleTime(){
-    return new Date().getTime() > lastRequestTime + throttleTime
+  function checkThrottleTime() {
+    return new Date().getTime() > lastRequestTime + throttleTime;
   }
-
 
   return (
-    <div className='app'>
+    <div className="app">
       <Statistics
         data={data}
         filter={filter}
         getFilteredData={getFilteredData}
         setFilter={setFilter}
       />
+      <SubmitForm handleAddData={handleAddData} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/SubmitForm.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { addTemperature } from "../firebase/firebase";
+import './SubmitForm.css'
+
+export default function SubmitForm({handleAddData}) {
+  const [temperature, setTemperature] = useState("");
+  const [location, setLocation] = useState("");
+  const [date, setDate] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    addTemperature(temperature, location, date);
+    setTemperature("");
+    setLocation("");
+    setDate("");
+    handleAddData()
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="submit" action="">
+      <h3>Add data</h3>
+      <div>
+        <label htmlFor="temperature">Temperature: </label>
+        <input
+          value={temperature}
+          required
+          onChange={(e) => setTemperature(+e.target.value)}
+          id="temperature"
+          type="number"
+        />
+      </div>
+      <div>
+        <label htmlFor="location">Location: </label>
+        <input
+          value={location}
+          required
+          onChange={(e) => setLocation(e.target.value)}
+          id="location"
+          type="text"
+        />
+      </div>
+      <div>
+        <label htmlFor="date">Date: </label>
+        <input
+          value={date}
+          required
+          onChange={(e) => setDate(e.target.value)}
+          id="date"
+          type="date"
+        />
+      </div>
+      <button>Submit</button>
+    </form>
+  );
+}
