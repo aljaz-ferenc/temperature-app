51413b278de697eeae4321cf81b18a361af74144
aljaz-ferenc <aljaz.ferenc@protonmail.com>
2023-04-28 10:50:52 +0200

    add fetch data on initial render functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import { useState } from 'react'
+import { useState, useMemo} from 'react'
 import './App.css'
+import { getAllTemps } from "./firebase/firebase";
+
 
 function App() {
+  const [data, setData] = useState(null);
+  const [lastRequestTime, setLastRequestTime] = useState(null)
+
+  //fetch on app render
+  useMemo(() => {
+    getAllData()
+    setLastRequestTime(currentTime())
+  }, []);
+
+  function getAllData(){
+    getAllTemps().then((data) => setData(data));
+  }
+
+  function currentTime(){
+    return new Date().getTime()
+  }
 
   return (
     <div className='app'></div>
